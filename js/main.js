e1d923f3b5e4cf5d1f112dfea8f83b700b34eb8a
nagn <mlin4589@gmail.com>
2015-03-26 15:32:51 -0400

    added car

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
-var path = new Path.Circle({
-	center: view.center,
-	radius: 30,
-	strokeColor: 'black'
-});
+function Car(x, y) {
+    var pos = new Point(x,y);
+    var speed = new Point(0,0);
+    var rotation = 0;
+    sprite = new Raster('car');
+    this.update = function(){
+        pos += speed;
+        //friction
+        speed*=0.96;
+        sprite.position = pos;
+    }
+    this.render = function() {
+        sprite.position = pos;
+        //sprite.rotation = rotation;
+    }
+
+}
+
+car = new Car(view.center.x, view.center.y);
 
 var frame = 0;
 function onFrame() {
+    /*
+    if (Key.isDown("left")){
+        car.steerLeft();
+    }
+    if (Key.isDown("right")){
+        car.steerRight();
+    }
+    */
+    car.render();
     frame += 0.01;
     frame %= 2 * Math.PI;
     
-    path.position.x = Math.cos(frame) * 100 + 100;
 }
 
 function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	path.position = view.center;
+	// Whenever the window is resized, recenter the path;
 }
